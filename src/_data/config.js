7d808b3ebfcf133d1d62ed4bd0a9b04c9b5550a7
Kyle Mitofsky <kylemit@gmail.com>
2020-03-05 18:37:13 -0500

    remove 2019

--- src/_data/config.js
+++ src/_data/config.js
@@ -18,8 +18,8 @@ module.exports = {
      * Base URL (protocol, domain)
      * For when fully qualified urls are needed.  
      * https://www.netlify.com/docs/continuous-deployment/#environment-variables
-     * Values:  https://www.vtcodecamp.org | https://deploy-preview-x--vtcodecamp2019.netlify.com/ 
-     *          https://branch--vtcodecamp2019.netlify.com | http://localhost:8080 
+     * Values:  https://www.vtcodecamp.org | https://deploy-preview-x--vtcodecamp.netlify.com/ 
+     *          https://branch--vtcodecamp.netlify.com | http://localhost:8080 
      */
     baseUrl: getBaseUrl(),
 

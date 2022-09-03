382e33b0cb50d6a0f526bd1c856ecf5d6727bfc1
Kyle <kmitofsky@stackoverflow.com>
2022-09-03 11:10:10 -0600

    remove meetupUrl

--- src/_data/config.js
+++ src/_data/config.js
@@ -8,7 +8,7 @@ const DEFAULT_DEV_URL = 'http://localhost:8080';
 module.exports = {
 
     eventDate: '9/24/2022',
-    meetupUrl: 'https://www.meetup.com/vtcode/events/287595613/',
+    meetupUrl: '',
 
     /**
      * Build Context
